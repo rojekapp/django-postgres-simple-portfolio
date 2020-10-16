a233a9ecd63896b0c3fcf1abd473f67f22f47cdb
unknown <mochammadrozaq16@gmail.com>
2020-10-16 19:45:15 +0700

    change directory database files

--- app/jobs/models.py
+++ app/jobs/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Job(models.Model):
     #image
-    image = models.ImageField(upload_to='mediafiles/')
+    image = models.ImageField(upload_to='databasefiles/')
 
     #summary
     summary = models.CharField(max_length=200)
